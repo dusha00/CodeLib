5611888767076430df93a97b13badd47575acf39
fw199006 <756091180@qq.com>
2016-03-02 16:26:18 +0800

    aop

---
+++ AOP/DynamicProx/Classlib.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.DynamicProxy;
+
+namespace TestOutput
+{
+    public interface IPlayable
+    {
+        void Play();
+    }
+
+    public class Animal : IPlayable
+    {
+        public virtual void Play()
+        {
+            Console.WriteLine("Animal.Play");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void Play()
+        {
+            Console.WriteLine("Dog.Play");
+        }
+    }
+
+    public interface IRunable
+    {
+        void Run();
+    }
+
+    public class RunAbility : IRunable
+    {
+        public void Run()
+        {
+            Console.WriteLine("RunAbility.Run");
+        }
+    }
+
+    public class AnimalInterceptor : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            Console.WriteLine("Before AnimalInterceptor.Intercept");
+            if (invocation.InvocationTarget != null)
+            {
+                invocation.Proceed();
+            }
+            Console.WriteLine("After AnimalInterceptor.Intercept");
+        }
+    }
+}
---
+++ AOP/DynamicProx/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.DynamicProxy;
+
+namespace TestOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n*************InterfaceProxyWithTargetInterface*************\n");
+            var generator = new ProxyGenerator();
+            var animal = generator.CreateInterfaceProxyWithTargetInterface<IPlayable>(new Dog(), new AnimalInterceptor());
+            animal.Play();
+            Console.WriteLine(animal.GetType());
+            Console.WriteLine(animal.GetType().BaseType);
+
+            var compositeField = animal.GetType().GetField("__target");
+            Console.WriteLine(compositeField);
+
+            foreach (var interfaceType in animal.GetType().GetInterfaces())
+            {
+                Console.WriteLine(interfaceType);
+            }
+            Console.Read();
+
+        }
+    }
+}
