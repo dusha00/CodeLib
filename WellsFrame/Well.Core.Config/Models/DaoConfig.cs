60246a93a262b9179094e0d9c0bafc4989c6e649
fw199006 <756091180@qq.com>
2016-03-04 14:22:23 +0800

    add wellsFrame

---
+++ WellsFrame/Well.Core.Config/Models/DaoConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Well.Core.Config.Models
+{
+    /// <summary>
+    /// 数据库配置
+    /// </summary>
+    [Serializable]
+    public class DaoConfig : ConfigFileBase
+    {
+        public DaoConfig()
+        {
+        }
+        #region 序列化属性
+        public String Account { get; set; }
+        public String Log { get; set; }
+        public String Cms { get; set; }
+        public String Crm { get; set; }
+        public String OA { get; set; }
+        #endregion
+    }
+}
