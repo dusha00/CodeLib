6ffa27c58abaf8345dae348887403396634012ea
756091180@qq.com <fw199006>
2016-05-21 21:46:57 +0800

    23 design

---
+++ DesignPattern/State/RequestA.cs
@@ -0,0 +1,10 @@
+﻿namespace State
+{
+    public class RequestA:State
+    {
+        public override void Handler(Context context)
+        {
+             context.State=new RequestA(); 
+        }
+    }
+}
